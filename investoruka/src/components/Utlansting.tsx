93f35b8f07de0cedcec8123f61247a91fd675ad5
ValCordDev <88210628+ValCordDev@users.noreply.github.com>
2023-11-22 11:08:23 +0100

    Updated "utlånsting"

--- investoruka/src/components/Utlansting.tsx
+++ investoruka/src/components/Utlansting.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
 
 const Utlansting = ({ item }: any) => {
-  console.log(item)
   return (
     <div className='bg-[#191919] rounded-2xl w-[32rem] h-48 shadow-xl flex flex-row'>
-      <img src="https://pricespy-75b8.kxcdn.com/product/standard/280/5113846.jpg" alt="Playstation" className='m-2 rounded-lg h-[11rem]' />
+      <img src={item.imgURL} alt="Playstation" className='m-2 rounded-lg h-[11rem] w-[11rem]' style={{ objectFit: 'cover' }} />
       <div className='text-lg my-4 ml-2 text-[#F4F4F4] font-light h-full w-full flex flex-col'>
-        <span>PlayStation 5</span>
+        <span>{item.title}</span>
+        <span className=' text-xs'>{item.description}</span>
         <div className='flex justify-between items-end h-full w-full pb-8 pr-5'>
-          <span>Status: <br/> 🟢 Ledig</span>
+          <span>Status: <br/>{!item.isRented ? <p>ikke leid</p> : <p>leid</p>}</span>
           <a className="inline-flex justify-center items-center gap-x-3 text-center bg-gradient-to-tl from-blue-600 to-violet-600 text-white text-sm font-medium rounded-md focus:outline-none focus:ring-1 py-3 px-4 focus:ring-offset-gray-800 w-30 h-10" href="#">
           Registrer
               <svg className="flex-shrink-0 w-4 h-4" width="16" height="16" viewBox="0 0 16 16" fill="none">
