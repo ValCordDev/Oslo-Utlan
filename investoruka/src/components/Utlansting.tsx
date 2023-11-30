3676962a2d5d50a2e20abf9529cfabf9ff18554b
ivermoka <ivermoka@icloud.com>
2023-11-30 11:28:21 +0100

    opptatt + kode (temp)

--- investoruka/src/components/Utlansting.tsx
+++ investoruka/src/components/Utlansting.tsx
@@ -103,6 +103,7 @@ const Utlansting = ({ item }: any) => {
     setReturnCode(e.target.value);
     console.log(returnCode);
   };
+  const isCurrentUserRenter = item.renter === uid;
 
   return (
     <div className="bg-[#191919] rounded-2xl w-[32rem] h-48 shadow-xl flex flex-row">
@@ -133,18 +134,24 @@ const Utlansting = ({ item }: any) => {
       <div className="text-lg my-4 ml-2 text-[#F4F4F4] font-light h-full w-full flex flex-col">
         <span>{item.title}</span>
         <span className=" text-xs pr-3">{item.description}</span>
+        {isCurrentUserRenter && (
+          <p className=" text-sm font-bold">Kode: {item.code}</p> // siden microbit ikke kan vise kode
+        )}
         <div className="flex justify-between items-end h-full w-full pb-8 pr-5">
           <span>
             Status: <br />
             {!item.isRented ? (
               <p>🟢 Ledig</p>
             ) : (
-              <p>🔴 Ikke ledig (ledig om {timeLeft?.daysLeft} dager)</p>
+              <p>
+                🔴 Ikke ledig <br /> {timeLeft?.daysLeft}d {timeLeft?.hoursLeft}
+                t {timeLeft?.minutesLeft}m
+              </p>
             )}
           </span>
 
-          {item.renter == uid ? ( // TODO: replace with actual user (with token + username in a verifyuser function)
-            <>
+          {item.isRented ? ( // TODO: replace with actual user (with token + username in a verifyuser function)
+            isCurrentUserRenter ? (
               <a
                 className="btn inline-flex justify-center items-center gap-x-3 text-center bg-gradient-to-tl from-blue-600 to-violet-600 text-white text-sm font-medium rounded-md py-3 px-4 focus:ring-offset-gray-800 w-30 h-10 active:scale-90 transition-all"
                 href="#"
@@ -166,7 +173,25 @@ const Utlansting = ({ item }: any) => {
                   />
                 </svg>
               </a>
-            </>
+            ) : (
+              <a className="btn inline-flex justify-center items-center gap-x-3 text-center bg-gray-500 text-white text-sm font-medium rounded-md py-3 px-4 focus:ring-offset-gray-800 w-30 h-10 active:scale-90 transition-all cursor-not-allowed">
+                Opptatt
+                <svg
+                  className="flex-shrink-0 w-4 h-4"
+                  width="16"
+                  height="16"
+                  viewBox="0 0 16 16"
+                  fill="none"
+                >
+                  <path
+                    d="M5.27921 2L10.9257 7.64645C11.1209 7.84171 11.1209 8.15829 10.9257 8.35355L5.27921 14"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                  />
+                </svg>
+              </a>
+            )
           ) : (
             <a
               className="btn inline-flex justify-center items-center gap-x-3 text-center bg-gradient-to-tl from-blue-600 to-violet-600 text-white text-sm font-medium rounded-md py-3 px-4 focus:ring-offset-gray-800 w-30 h-10 active:scale-90 transition-all"
